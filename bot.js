96fc4ec545abe0965378c0558b95290edb629127
BeachBabes <49539149+BeachBabes@users.noreply.github.com>
2020-02-20 18:39:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('guildMemberAdd', function(member)
     .addField('Incase you leave and want to come back!', 'https://discord.gg/4r5NV7k')
     .setThumbnail('https://cdn.discordapp.com/attachments/559520022951755780/571798911031705600/sansberry.jpg')
     member.send(join)
-    let memberRole = member.guild.roles.find('name', 'Bro')
+    let memberRole = member.guild.roles.find('name', 'Visitor')
     member.addRole(memberRole)
 })
 client.on('guildMemberRemove', (member) =>
